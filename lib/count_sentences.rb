c3a07c9e78d3e339410b150ba01a78d3baf1d2f0
alec <kaltschmidalec@gmail.com>
2021-08-31 21:30:37 -0700

    commit tree 4423cbef39befe385682a8a0e839ac6a01a820f5

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.include?(".")
   end
 
   def question?
-
+    self.include?("?")
   end
 
   def exclamation?
-
+    self.include?("!")
   end
 
   def count_sentences
+    num_sentences = 0
+    splits = [".", "?", "!"]
 
+    self.each_char.with_index do |char, index|
+      splits.each do |split|
+        if (split == char) && ((self[index + 1]) != split)
+          num_sentences += 1
+        end
+      end
+    end
+    num_sentences
+
+    # count_sentences = self.split("." || "?" || "!")
+    # num_sentences = count_sentences.length
+    # num_sentences
+    # binding.pry
   end
 end
